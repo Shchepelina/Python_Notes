00f548da9efc12f258f79ab9a432625867d86c90
shchepelina <apt_dobdoctora@mail.ru>
2023-04-17 11:16:53 +0300

    Python_Notes

--- Note.py
+++ Note.py
@@ -1,25 +1,26 @@
 from datetime import datetime
 import uuid
 
+
 class Note:
-    def __init__(self, id = str(uuid.uuid1())[0:3], title = 'текст', body = 'текст', date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
-        self.id = self
+    def __init__(self, id = str(uuid.uuid1())[0:3],  title = "текст", body = "текст", date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
+        self.id = id
         self.title = title
         self.body = body
         self.date = date
 
     def get_id(note):
         return note.id
-    
-    def get_tittle(note):
+
+    def get_title(note):
         return note.title
-    
+
     def get_body(note):
         return note.body
-    
+
     def get_date(note):
         return note.date
-    
+
     def set_id(note):
         note.id = str(uuid.uuid1())[0:3]
 
@@ -37,8 +38,3 @@ class Note:
 
     def map_note(note):
         return '\nID: ' + note.id + '\n' + 'Название: ' + note.title + '\n' + 'Описание: ' + note.body + '\n' + 'Дата публикации: ' + note.date
-
-
-
-
-    
